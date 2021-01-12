855504956ffeda6f8d6bc9a69258f0a7d1e6d347
gamedev035 <76887794+gamedev035@users.noreply.github.com>
2021-01-12 15:15:24 +0100

    The second episode on how to create minecraft with unity
    In thIS episode 2  on how to create minecraft with unity we generated the terrain with the script  World.cs from the assets of this repository

---
+++ scripts/world.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class world : MonoBehaviour
+{
+    //terrain variables
+    public GameObject block;
+
+    public int cols = 16;
+    public int rows = 16;
+    
+    public float freq = 10f;
+    public float amp = 10f;
+
+    
+    void Update()
+    {
+        //terrain loop
+        for (int x = 0; x < cols; x++)
+        {
+            for (int z = 0; z < rows; z++)
+            {
+                float y = Mathf.PerlinNoise(x / freq, z/freq) * amp;
+
+                y = Mathf.Round(y);
+                
+                for (int h = 0; h < y; h++)
+                {
+                    Instantiate(block, new Vector3(x, h, z), Quaternion.identity);
+                }
+            }
+        }
+    }
+}
